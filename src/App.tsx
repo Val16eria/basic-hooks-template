e6d790b2aecb7a952299955b9f32de6ce5c30a83
Val16eria <KStorer61@icloud.com>
2023-02-21 14:20:14 +0300

    update input useState

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import {InputUseState} from "./components/hooks-template/useState-useReducer/InputUseState";
+
+function App() {
+  return (
+    <div className="App">
+        <h1>useState</h1>
+        <InputUseState />
+    </div>
+  );
+}
+
+export default App;
